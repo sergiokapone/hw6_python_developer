c71636c9e4dbc3efad56dc99e93d121389455a07
Sergiy <sergiy.ponomarenko@gmail.com>
2023-02-03 09:57:28 +0200

    refactor

--- file_sorter.py
+++ file_sorter.py
@@ -1,167 +1,9 @@
-from os import listdir, makedirs, rmdir, rename
-from os.path import isdir, isfile, splitext, basename, exists, join
 import shutil
 import sys
+from pathlib import Path
+from normaliser import normalise
 
-
-"""
-Скрипт приймає один аргумент при запуску — це ім'я папки,
-в якій він буде проводити сортування.
-Допустимо файл з програмою називається
-sort.py, тоді, щоб відсортувати папку /user/Desktop/Мотлох,
-треба запустити скрипт командою python sort.py /user/Desktop/Мотлох
-
-В результатах роботи повинні бути:
-
-Список файлів в кожній категорії (музика, відео, фото и ін.)
-Перелік усіх відомих скрипту розширень, які зустрічаються в цільовій папці.
-Перелік всіх розширень, які скрипту невідомі.
-Після необхідно додати функції, які будуть
-відповідати за обробку кожного типу файлів.
-
-Крім того, всі файли та папки треба перейменувати,
-видалив із назви всі символи, що призводять до проблем.
-Для цього треба застосувати до імен файлів функцію normalize.
-Слід розуміти, що перейменувати файли треба так,
-щоб не змінити розширень файлів.
-
-Функція normalize:
-
-Проводить транслітерацію кирилічного алфавіту на латинський.
-Замінює всі символи крім латинських літер, цифр на '_'.
-Вимоги до функції normalize:
-
-приймає на вхід рядок та повертає рядок;
-проводить транслітерацію кирилічних символів на латиницю;
-замінює всі символи, крім літер латинського алфавіту та цифр, на символ '_';
-транслітерація може не відповідати стандарту, але бути читабельною;
-великі літери залишаються великими,
-а маленькі — маленькими після транслітерації.
-
-"""
-
-
-PATH = sys.argv[1]
-
-
-""" ========================== Таблиця транслітерації ======================"""
-
-
-TRANS = {'а': 'a',
- 'А': 'A',
- 'б': 'b',
- 'Б': 'B',
- 'в': 'v',
- 'В': 'V',
- 'г': 'g',
- 'Г': 'G',
- 'д': 'd',
- 'Д': 'D',
- 'е': 'e',
- 'Е': 'E',
- 'ё': 'e',
- 'Ё': 'E',
- 'ж': 'j',
- 'Ж': 'J',
- 'з': 'z',
- 'З': 'Z',
- 'и': 'i',
- 'И': 'I',
- 'й': 'j',
- 'Й': 'J',
- 'к': 'k',
- 'К': 'K',
- 'л': 'l',
- 'Л': 'L',
- 'м': 'm',
- 'М': 'M',
- 'н': 'n',
- 'Н': 'N',
- 'о': 'o',
- 'О': 'O',
- 'п': 'p',
- 'П': 'P',
- 'р': 'r',
- 'Р': 'R',
- 'с': 's',
- 'С': 'S',
- 'т': 't',
- 'Т': 'T',
- 'у': 'u',
- 'У': 'U',
- 'ф': 'f',
- 'Ф': 'F',
- 'х': 'h',
- 'Х': 'H',
- 'ц': 'ts',
- 'Ц': 'TS',
- 'ч': 'ch',
- 'Ч': 'CH',
- 'ш': 'sh',
- 'Ш': 'SH',
- 'щ': 'sch',
- 'Щ': 'SCH',
- 'ъ': '',
- 'Ъ': '',
- 'ы': 'y',
- 'Ы': 'Y',
- 'ь': '',
- 'Ь': '',
- 'э': 'e',
- 'Э': 'E',
- 'ю': 'yu',
- 'Ю': 'YU',
- 'я': 'ya',
- 'Я': 'YA',
- 'є': 'je',
- 'Є': 'JE',
- 'і': 'i',
- 'І': 'I',
- 'ї': 'ji',
- 'Ї': 'JI',
- 'ґ': 'g',
- 'Ґ': 'G'}
-
-""" ==================== Таблиця кодівань =================================="""
-
-
-# https://www.w3schools.com/charsets/ref_utf_cyrillic.asp
-
-LATIN_CODES = tuple(range(65, 91)) + tuple(range(97, 123))
-
-CYR_CODES = tuple(range(1024, 1280))
-
-OTHER_SYMBOLS = tuple(str(x) for x in range(0, 10)) + tuple()
-
-WILDCARD = '_'
-
-""" ======================== Функця нормалізації ==========================="""
-
-
-def normalize(file_name):
-    """Функція normalize - проводить транслітерацію кирилічного алфавіту.
-
-    - Приймає на вхід рядок - ім'я файлу (без розширення) та повертає
-    нормалізований рядок.
-    - Танслітерує кирилчну назву в латиницю:
-      -- транслітерація може не відповідати стандарту, але бути читабельною;
-      -- великі літери залишаються великими;
-      -- маленькі залишаються маленькими після транслітерації.
-    - Замінює всі символи крім латинських літер, цифр на '_'.
-
-    """
-
-    trans = ""  # ініціалізаці
-
-    for letter in file_name:
-        if ord(letter) in LATIN_CODES or letter in OTHER_SYMBOLS:
-            trans += letter
-        elif ord(letter) in CYR_CODES:
-            trans += TRANS.get(letter)
-        else:
-            trans += WILDCARD
-
-    return trans
+PATH = Path(sys.argv[1])
 
 
 """ =============== Таблиця відаовідності папок і розширень ================"""
@@ -187,133 +29,102 @@ EXTS = EXT_FOLDER.keys()
 """ ============================= Функці =================================="""
 
 
-def separate_file_name_ext(path, item):
-    """Функція повертає ім'я та розширення файлу (у верхньому регістрі).
-
-    На вхід подається шлях до файлу, та його ім'я зрозширенням.
-    Якщо на вхід подається імя папки, то повертає кортеж двох порожніх рядків.
-    """
-
-    full_item_path = join(path, item)
-
-    if isfile(full_item_path):
-
-        name, ext = splitext(basename(full_item_path))
-
-        return name, ext.lstrip('.').upper()
-
-    return "", ""
-
-
 def create_folders(path):
     """Функція створює у КАТАЛОЗІ папки відповідно до їх розширення.
 
-    Приймає шлях до КАТАЛОГУ.
-
-    Дані про відповідність розширення і каталогу
-    знаходяться в словнику EXT_FOLDER.
     """
+    the_path = Path(path)
 
-    current_folder_exts = set()
+    files_exts = set()
 
-    for item in listdir(path):
+    for item in the_path.iterdir():
 
-        file_ext = separate_file_name_ext(path, item)[1]
+        if item.is_file():
 
-        current_folder_exts.add(file_ext)
+            files_exts.add(item.suffix.lstrip('.').upper())
 
-    for ext in current_folder_exts:
+    for ext in files_exts:
 
         for key in EXTS:
 
             if ext in key:
 
-                if not exists(join(path, EXT_FOLDER[key])):
+                cathegory_path = Path(the_path.joinpath(EXT_FOLDER[key]))
 
-                    makedirs(join(path, EXT_FOLDER[key]))
+                cathegory_path.mkdir(exist_ok=True)
 
 
-def move_file(path, file_name_ext):
+def move_file(file):
     """Функція переміщує файли до відповідних каталогів.
 
-    Приймає шлях до КАТАЛОГУ і та ім'я файлу з розширенням.
-
-    Дані про відповідність розширення і каталогу
-    знаходяться в словнику FOLDERS.
     """
 
-    file_name, file_ext = separate_file_name_ext(path, file_name_ext)
+    the_path = Path(file)
 
-    full_path_to_file = join(path, file_name_ext)
+    ext = the_path.suffix.lstrip('.').upper()
+
+    name = the_path.name
+
+    path = the_path.parent
 
     for key in EXTS:
 
-        if file_ext in key:
+        if ext in key:
 
-            shutil.move(full_path_to_file,
-                        join(path, EXT_FOLDER[key]))
+            shutil.move(file, Path(path).joinpath(EXT_FOLDER[key]))
 
-            if file_ext in ARCHIVES:
+            if ext in ARCHIVES:
 
-                full_path_to_arj_file = join(path, EXT_FOLDER[key])
-
-                unpack(
-                       join(full_path_to_arj_file, file_name_ext),
-                       join(full_path_to_arj_file, file_name)
-                        )
+                unpack(Path(path).joinpath(EXT_FOLDER[key], name),
+                       Path(path).joinpath(EXT_FOLDER[key], file.stem))
 
 
-def normalise_file_name(path, old_name_ext):
+def normalise_file_name(file):
     """Функція перейменовує файли відповідно до таблиці транслітерації TRANS.
-
-    Приймає шлях до файлу та його ім'я з розширенням.
 
     """
 
-    old_name, ext = separate_file_name_ext(path, old_name_ext)
+    the_path = Path(file)
 
-    new_name = normalize(old_name)
+    normalised_name = normalise(the_path.stem)
 
-    new_name_ext = ".".join([new_name, ext.lower()])
+    new_file_path = the_path.parent.joinpath("".join([normalised_name, the_path.suffix]))
 
-    full_path_old_name_file = join(path, old_name_ext)
+    the_path.rename(new_file_path)
 
-    full_path_new_name_file = join(path, new_name_ext)
-
-    rename(full_path_old_name_file, full_path_new_name_file)
+    return new_file_path
 
 
 def sort_dir(path):
     """Функція фасує файли по відповідним папкам.
 
-    Приймає шлях до КАТАЛОГУ.
-
     """
 
-    if len(listdir(path)) == 0:
+    the_path = Path(path)
 
-        rmdir(path)
+    if not any(the_path.iterdir()):
+
+        the_path.rmdir()
 
     else:
 
-        for item in listdir(path):
+        for item in the_path.iterdir():
 
-            if isdir(join(path, item)) and\
-                    item not in FOLDERS:
+            if item.is_dir() and item.name not in FOLDERS:
 
-                sort_dir(join(path, item))
+                sort_dir(the_path.joinpath(item.name))
 
             else:
 
                 create_folders(path)
 
-                for item in listdir(path):
+                for item in the_path.iterdir():
 
-                    if isfile(join(path, item)):
+                    if item.is_file():
 
-                        normalise_file_name(path, item)
+                        item = normalise_file_name(item)
 
-                        move_file(path, item)
+                        move_file(item)
 
 
 def unpack(archive_path, path_to_unpack):
@@ -324,7 +135,7 @@ def unpack(archive_path, path_to_unpack):
 
         shutil.unpack_archive(archive_path, path_to_unpack)
 
-    except (OSError, IOError):
+    except OSError:
 
         print(f"Unsupported file format {archive_path}")
 
@@ -334,15 +145,13 @@ def unpack(archive_path, path_to_unpack):
 
 if __name__ == '__main__':
 
-    path = PATH
-
     agreement = input(
-        f'УВАГА! Ви впевнені, що шочере сортувати файли в КАТАЛОЗІ {path}? (y/n):'
+        f'УВАГА! Ви впевнені, що хочере сортувати файли в КАТАЛОЗІ {PATH}? (y/n): '
         )
 
     if agreement in ('y', 'Y', 'н', 'Н'):
 
-        sort_dir(path)
+        sort_dir(PATH)
 
         input('Операція успішно завершена! Натисніть довільну клавішу')
 
